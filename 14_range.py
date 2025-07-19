3edf3f8dbf13f5ef4de35af978f269f619fc813e
ifagatbuse <162689954+ifagatbuse@users.noreply.github.com>
2025-07-19 15:25:57 +0300

    Add files via upload

---
+++ 14_range.py
@@ -0,0 +1,3 @@
+for sayi in range(1,11):
+    print("sayi:",sayi)
+    print("square:",sayi*sayi)
---
+++ 15_conditional_operationonthelist.py
@@ -0,0 +1,9 @@
+
+numbers = [5, 8, 12, 3, 18, 7]
+
+
+for number in numbers:
+    
+    if number > 10:
+     
+        print("Number greater than 10:", number)
