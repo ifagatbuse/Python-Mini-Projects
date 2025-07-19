e7d4c97928d8333164b463abcf2b49d39e7d969d
ifagatbuse <162689954+ifagatbuse@users.noreply.github.com>
2025-07-19 15:25:37 +0300

    Add files via upload

---
+++ 13_movie.py
@@ -0,0 +1,13 @@
+
+movies = ["Inception", "Interstellar", "Matrix", "Tenet", "Avatar"]
+
+
+for movie in movies:
+    print("My favorite movie:", movie)
+
+
+numbers = [1, 2, 3, 4, 5]
+
+
+for n in numbers:
+    print("Double:", n * 2)
